c1c126883a3cbe42932c5a81d05e261e2349ac5d
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-07-30 12:36:01 +0900

    mypage CSS 수정

--- src/assets/js/mypage.js
+++ src/assets/js/mypage.js
@@ -1,6 +1,11 @@
+import { getCookie } from "./util.js";
+
 const $nickname = document.querySelector('.user-name')
 const $aboutMe = document.querySelector('.user-description')
 const $avatar_img = document.querySelector('.user-avatar')
+
+// Django Server URL
+const url = `http://127.0.0.1:8000/chatbot/mylist/`;
 
 const profile_setting = () => {
     let user = sessionStorage.getItem('user');
@@ -16,4 +21,34 @@ const profile_setting = () => {
     }
 }
 
+// Django Server와 통신
+const myListLoad = async () => {
+    const access = getCookie('access')
+
+    const result = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${access}`,
+        },
+        redirect: "follow",
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.data.length < 1) {
+                console.log('게시물이 존재하지 않습니다.')
+            } else {
+                (res.data).forEach(element => {
+                    console.log(element)
+                });
+            }
+            
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+
 profile_setting()
+myListLoad()
